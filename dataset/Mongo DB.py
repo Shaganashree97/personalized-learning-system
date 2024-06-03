f51ba28abbb916378d5cf1c38606ea616c3efad7
Kavya Prabahar <144550263+kavya-prabahar@users.noreply.github.com>
2024-06-03 19:10:55 +0530

    Update Mongo DB.py
    Two features namely "Article" and "Youtube" has been created which consists of a article link and youtube link for each topic respectively.

--- dataset/Mongo DB.py
+++ dataset/Mongo DB.py
@@ -9,45 +9,33 @@ collection = db['content_collection']
 data = [
     {
         "topic": "Percentages",
-        "links": [
-            "https://www.geeksforgeeks.org/percentage-aptitude-questions/",
-            "https://youtu.be/RWdNhJWwzSs"
-        ]
+        "Article":"https://www.geeksforgeeks.org/percentage-aptitude-questions/",
+        "Youtube":"https://youtu.be/RWdNhJWwzSs"
     },
     {
         "topic": "Boats and streams",
-        "links": [
-            "https://youtu.be/yp2tgfwT3YU",
-            "https://www.bing.com/search?q=boats+and+streams&FORM=HDRSC1"
-        ]
+        "Article":"https://www.bing.com/search?q=boats+and+streams&FORM=HDRSC1",
+        "Youtube":"https://youtu.be/yp2tgfwT3YU"
     },
     {
         "topic": "Age problems",
-        "links": [
-            "https://youtu.be/b9YMbDH02rM",
-            "https://byjus.com/govt-exams/age-problems/"
-        ]
+        "Article":"https://byjus.com/govt-exams/age-problems/",
+        "Youtube": "https://youtu.be/b9YMbDH02rM"
     },
     {
         "topic": "Time, distance, speed",
-        "links": [
-            "https://byjus.com/speed-distance-time-formula/",
-            "https://youtu.be/dHVK7IeLGT8"
-        ]
+        "Article":"https://byjus.com/speed-distance-time-formula/",
+        "Youtube":"https://youtu.be/dHVK7IeLGT8"
     },
     {
         "topic": "Number series",
-        "links": [
-            "https://www.geeksforgeeks.org/number-series-in-quantitative-aptitude/",
-            "https://youtu.be/iwpd_RkG-1U"
-        ]
+        "Article":"https://www.geeksforgeeks.org/number-series-in-quantitative-aptitude/",
+        "Youtube":"https://youtu.be/iwpd_RkG-1U"
     },
     {
         "topic": "Pipes and cisterns",
-        "links": [
-            "https://byjus.com/govt-exams/pipes-cistern-questions/",
-            "https://youtu.be/AHEFRed92zM"
-        ]
+        "Article": "https://byjus.com/govt-exams/pipes-cistern-questions/",
+        "Youtube":"https://youtu.be/AHEFRed92zM"
     }
 ]
 
